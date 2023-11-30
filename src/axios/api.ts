53c799e3bdc6afe68a4fbd9e09bc21c2ea024460
hyun0509-iva <skyblue5030@gmail.com>
2023-11-30 21:50:16 +0900

    chore: BASE_URL 환경 변수 설정

--- src/axios/api.ts
+++ src/axios/api.ts
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
+console.log("msg", process.env.BASE_URL ?? " http://localhost:3000/api");
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.BASE_URL || 'http://localhost:3000/api',
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Cache-Control": "no-cache",
-  }
-})
+  },
+});
