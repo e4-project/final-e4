5a856698b12f8b6b810bd10bb4b309cc46f9c194
Dong-Hoon94 <kane5178@naver.com>
2023-12-02 05:27:14 +0900

    feat: 이미지캐러셀 구현

--- src/components/ImgSlider.tsx
+++ src/components/ImgSlider.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image from "next/image";
 
 const ImgSlider = () => {
   const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
+    arrows:false,           // 양 옆에있는 next prev button
+    dots: true,             // 하단에 있는 동그라미 버튼
+    infinite: true,         // 무한반복 
+    speed: 500,             // 슬라이드 넘어갈때 속도 
+    slidesToShow: 1,        //한 번에 볼 수 있는 슬라이드 개수
+    slidesToScroll: 1,      // 한 번에 넘어가는 슬라이드 수 
+    autoplay: true,        //자동으로 슬라이드 넘어가게 해줌
+    autoplaySpeed: 2500,    // 위 속도
   };
 
   return (
@@ -22,7 +22,8 @@ const ImgSlider = () => {
           src="img/1.png"
           style={{
             width: "100%",
-            height: "400px",
+            height: "350px",
+  
           }}
         />
       </div>
@@ -31,7 +32,7 @@ const ImgSlider = () => {
           src="img/2.png"
           style={{
             width: "100%",
-            height: "400px",
+            height: "350px",
           }}
         />
       </div>
@@ -40,7 +41,7 @@ const ImgSlider = () => {
           src="img/3.png"
           style={{
             width: "100%",
-            height: "400px",
+            height: "350px",
           }}
         />
       </div>
@@ -49,7 +50,7 @@ const ImgSlider = () => {
           src="img/4.png"
           style={{
             width: "100%",
-            height: "400px",
+            height: "350px",
           }}
         />
       </div>
