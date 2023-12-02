4704361ed29d7e337b5dda80290ca716eeb6f84e
catmaker <rzt0317@nate.com>
2023-12-03 07:04:24 +0900

    feat: github 로그인 세션 생성 및 세션 로그 출력 / 로그인 컴포넌트 분할

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./LoginModal.module.css";
+import KaKaoLoginBtn from "./KaKaoLoginBtn";
+import GithubLoginBtn from "./GithubLoginBtn";
 
 interface LoginModalProps {
   onClose: () => void;
 }
 
 const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
+  const [session, setSession] = useState(null);
+  useEffect(() => {
+    fetch("/api/session")
+      .then((response) => response.json())
+      .then((data) => setSession(data.session))
+      .catch((error) => console.error("Error:", error));
+  }, []);
   const handleOutsideClick = (e: React.MouseEvent) => {
     if (e.target === e.currentTarget) {
       onClose();
@@ -20,12 +29,9 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
           onClick={(e) => e.stopPropagation()}
         >
           <h1 className={style.h1}>간편하게 시작해보세요</h1>
-          <div className={`${style.login_wrapper} ${style.kakao_bg}`}>
-            <p>카카오 로그인</p>
-          </div>
-          <div className={`${style.login_wrapper} ${style.github_bg}`}>
-            <p className={style.white}>GitHub 로그인</p>
-          </div>
+          {session && <p>Session: {JSON.stringify(session)}</p>}
+          <KaKaoLoginBtn />
+          <GithubLoginBtn />
         </div>
       </div>
     </div>
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "d89cc9b0b347e69a7ad3",
+      clientSecret: "640a92b95fdcceaf43a00f3fba37ff1db68a7a15",
+    }),
+  ],
+  secret: "qwer1234",
+};
+export default NextAuth(authOptions);
