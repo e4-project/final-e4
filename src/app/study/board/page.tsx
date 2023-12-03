623aeebbced178c1cf7cf2d9f203474947545abf
hyun0509-iva <skyblue5030@gmail.com>
2023-12-03 23:33:15 +0900

    style: IResponseBoard type 경로 수정

--- src/app/study/board/page.tsx
+++ src/app/study/board/page.tsx
@@ -1,6 +1,7 @@
 // 프론트 서버
 import { loadBoardApi } from "@/axios/fetcher/board/loadBoardApi";
-import BoardView, { IResponseBoard } from "./page.view";
+import BoardView from "./page.view";
+import { IResponseBoard } from "@/interfaces/study_board";
 
 const Page = async () => {
   try {
