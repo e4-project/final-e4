393ee2a01678bd867529b4fb2a5c3fc2a1af9c17
yuio12 <loadsi12@naver.com>
2023-12-04 17:48:38 +0900

    design:css수정 및 드롭다운 클릭기능 수정과 알림 list 개수 감지기능 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import style from "./header.module.css";
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 /**
  * @name header
@@ -12,24 +12,55 @@ import { useRef, useEffect } from 'react';
  */
 
 const Header = () => {
+
+  // 외부 영역 클릭시 드롭다운창 닫기
+  const bellRef = useRef<HTMLInputElement | null>(null);
   const searchRef = useRef<HTMLInputElement | null>(null);
-  
 
   useEffect(() => {
   	function handleFocus(e:any) {
       const bell = document.getElementById('bell') as HTMLInputElement;
-      const profile = document.getElementById('profile') as HTMLInputElement;
-      	if (searchRef.current && bell && profile) {
-          if(!searchRef.current.contains(e.target)) {
+      	if (bellRef.current && bell) {
+          if(!bellRef.current.contains(e.target)) {
             bell.checked = false;
-            profile.checked = false;
           }
         }
       }
       
-      document.addEventListener("mouseup", handleFocus);
-      return () => { document.removeEventListener("mouseup", handleFocus); }
+      document.addEventListener("click", handleFocus);
+      return () => { document.removeEventListener("click", handleFocus); }
+  }, [bellRef]);
+  // 
+  useEffect(() => {
+  	function handleFocus(e:any) {
+      const profile = document.getElementById('profile') as HTMLInputElement;
+      	if (searchRef.current && profile) {
+          if(!searchRef.current.contains(e.target)) {
+            profile.checked = false;
+          }
+        }
+        
+      }
+      
+      document.addEventListener("click", handleFocus);
+      return () => { document.removeEventListener("click", handleFocus); }
   }, [searchRef]);
+
+  // 알림 개수 보여주는 코드
+  const [liCount, setLiCount] = useState(0);
+  useEffect(()=>{
+    const updateLiCount = ()=>{
+      const list = document.querySelectorAll('.push_list');
+      setLiCount(list.length);
+    };
+    updateLiCount();
+    const observer = new MutationObserver(updateLiCount);
+    const config = { childList: true, subtree: true };
+    observer.observe(document.body, config);
+    return ()=>observer.disconnect(); 
+  }, [])
+  
+
 
   return (
     <div className={style.bar}>
@@ -47,32 +78,42 @@ const Header = () => {
         </ul>
         <div>
           <div className={style.profile_title}>
-            <input ref={searchRef} type="checkbox" name="profile" id="bell" className={style.bell_check} />
+            {/*  */}
+            <input ref={bellRef} type="checkbox" name="bell" id="bell"
+             className={style.bell_check}
+            />
             <label htmlFor="bell" className={style.bell}>
-              <img src="/Frame 59.png" alt="" />
+              {liCount === 0 ? (
+              <span className={style.bell_length} style={{display: 'none'}}>{liCount}</span>)
+              :(<span className={style.bell_length}>{liCount}</span>)}
+              <img src="/icons/icon_bell.svg" alt="" />
+              <ul className={style.bell_menu}>
+                <li className="push_list">알림내용</li>
+                <li className="push_list">알림내용</li>
+              </ul>
             </label>
-
-            <input type="checkbox" name="profile" id="profile" className={style.profile_check}/>
+  
+            {/*  */}
+            <input ref={searchRef} type="checkbox" name="profile" id="profile"
+             className={style.profile_check}
+            />
             <label htmlFor="profile" className={style.profile}>
-
+              <ul className={style.profile_menu}>
+                <li className={style.profile_menu_frame}>
+                  <div className={style.profile_menu_img}></div>
+                  <ul className={style.profile_menu_sheet}>
+                    <span className={style.profile_menu_name}>닉네임</span>
+                    <li><Link href="/Studypage">My Study</Link></li>
+                    <li>마이페이지</li>
+                    <li>내 스터디</li>
+                    <li>설정</li>
+                    <li>로그아웃/로그인</li>
+                  </ul>
+                </li>
+              </ul>
             </label>
-            <ul className={style.bell_menu}>
-              <li>알림내용</li>
-              <li>알림내용</li>
-            </ul>
-            <ul className={style.profile_menu}>
-              <li className={style.profile_menu_frame}>
-                <div className={style.profile_menu_img}></div>
-                <ul className={style.profile_menu_sheet}>
-                  <li className={style.profile_menu_name}>닉네임</li>
-                  <li><Link href="/Studypage">My Study</Link></li>
-                  <li>마이페이지</li>
-                  <li>내 스터디</li>
-                  <li>설정</li>
-                  <li>로그아웃/로그인</li>
-                </ul>
-              </li>
-            </ul>
+
+            {/*  */}
           </div>
         </div>
       </div>
