4ca39aa30596f80c0125a2045e8f5bac46c2e6a0
catmaker <rzt0317@nate.com>
2023-12-05 14:48:02 +0900

    feat : 카카오 로그인 기능 추가 키는 나중에 환경 변수로 빼야함

--- src/components/LoginModal/KaKaoLoginBtn.tsx
+++ src/components/LoginModal/KaKaoLoginBtn.tsx
@@ -3,7 +3,10 @@ import { signIn } from "next-auth/react";
 import style from "./LoginModal.module.css";
 export default function LoginBtn() {
   return (
-    <button className={`${style.login_wrapper} ${style.kakao_bg}`}>
+    <button
+      onClick={() => signIn("kakao")}
+      className={`${style.login_wrapper} ${style.kakao_bg}`}
+    >
       <p>카카오 로그인</p>
     </button>
   );
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,12 +2,16 @@ import connectDB from "@/config/db/connectDB";
 import { MongooseAdapter } from "@choutkamartin/mongoose-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
+import KakaoProvider from "next-auth/providers/kakao";
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: "d89cc9b0b347e69a7ad3",
       clientSecret: "640a92b95fdcceaf43a00f3fba37ff1db68a7a15",
+    }),
+    KakaoProvider({
+      clientId: "99eff536faec341d98a8471e71b08e27",
+      clientSecret: "ZA2cghpHGva3DpwFGFtZeZrW02eRbbMp",
     }),
   ],
   adapter: MongooseAdapter(process.env.MONGODB_URI),
