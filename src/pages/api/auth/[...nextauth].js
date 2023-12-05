aaa75294f88d47582fa0422bab18b0eddcdbe4cc
catmaker <rzt0317@nate.com>
2023-12-05 09:41:31 +0900

    feat: 몽구스 어댑터 db 연결 추가

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,13 +4,13 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-  adapter: MongooseAdapter(process.env.MONGODB_URI),
   providers: [
     GithubProvider({
       clientId: "d89cc9b0b347e69a7ad3",
       clientSecret: "640a92b95fdcceaf43a00f3fba37ff1db68a7a15",
     }),
   ],
+  adapter: MongooseAdapter(process.env.MONGODB_URI),
   secret: "qwer1234",
 };
 export default NextAuth(authOptions);
