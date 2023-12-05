30f7aab5c3e7cc2fbf3f17f0088e7f2a3b987948
catmaker <rzt0317@nate.com>
2023-12-05 15:11:28 +0900

    feat: 구글 로그인 기능 추가 (환경변수 추후 변경)

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,9 @@
-import connectDB from "@/config/db/connectDB";
 import { MongooseAdapter } from "@choutkamartin/mongoose-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import KakaoProvider from "next-auth/providers/kakao";
+import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions = {
   providers: [
     GithubProvider({
@@ -13,8 +14,14 @@ export const authOptions = {
       clientId: "99eff536faec341d98a8471e71b08e27",
       clientSecret: "ZA2cghpHGva3DpwFGFtZeZrW02eRbbMp",
     }),
+    GoogleProvider({
+      clientId:
+        "596916716907-sfa1p3mj7c4bfns921obipclfch5vh22.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
   ],
   adapter: MongooseAdapter(process.env.MONGODB_URI),
   secret: "qwer1234",
 };
+
 export default NextAuth(authOptions);
