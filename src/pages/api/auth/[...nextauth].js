81fc2cbcdf3ca98ee219e78962daeae829a944e6
catmaker <rzt0317@nate.com>
2023-12-05 15:21:01 +0900

    fix : Provider id 환경 변수로 대체

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,17 +7,16 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "d89cc9b0b347e69a7ad3",
-      clientSecret: "640a92b95fdcceaf43a00f3fba37ff1db68a7a15",
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     KakaoProvider({
-      clientId: "99eff536faec341d98a8471e71b08e27",
-      clientSecret: "ZA2cghpHGva3DpwFGFtZeZrW02eRbbMp",
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
     GoogleProvider({
-      clientId:
-        "596916716907-sfa1p3mj7c4bfns921obipclfch5vh22.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   adapter: MongooseAdapter(process.env.MONGODB_URI),
