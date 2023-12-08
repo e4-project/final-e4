a831596f61bd975c951f7070b54624fea1ea0853
hyun0509-iva <skyblue5030@gmail.com>
2023-12-09 00:31:04 +0900

    feat: 댓글 컴포넌트 fetcher 함수를 파라미터로 추가  : 댓글 컴포넌트의 재사용성을 위해 fetcher 함수를 외부에서 받아서 데이터를 전달할 수 있게 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import RecruitView from "./page.view";
 import { loadRecruitApi } from "@/axios/fetcher/recruit/loadRecruitApi";
-import { IRequestRecruitPost } from "@/interfaces/recruit";
+import { IResponseRecruitPost } from "@/interfaces/recruit";
 
 const Page = async () => {
-  const data: IRequestRecruitPost[] = await loadRecruitApi();
+  const data: IResponseRecruitPost[] = await loadRecruitApi();
   return (
     <div>
       <RecruitView data={data} />
