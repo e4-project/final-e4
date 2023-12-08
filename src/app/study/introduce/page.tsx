df66e10b726de4c856f9f51ee6de63f7be344058
RangCloud <mtr008@naver.com>
2023-12-08 10:16:56 +0900

    feat: 스터디 정보 db 넣기

--- src/app/study/introduce/page.tsx
+++ src/app/study/introduce/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import style from './introduce.module.css';
+import IntroduceView from './page.view';
 
 const page = () => {
   return (
-    <div className={style.intro_container}>
-      <div>스터디 소개글 받아오기</div>
-    </div>
+    <IntroduceView />
   )
 }
 
