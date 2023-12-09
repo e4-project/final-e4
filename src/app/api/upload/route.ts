81d968caa9f160c2addacdcbe286fe57bd6af277
catmaker <rzt0317@nate.com>
2023-12-10 04:45:34 +0900

    feat : 마이페이지 생성 및 기능 추가 (세션 업데이트 추가)

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,31 +1,33 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../../pages/api/auth/[...nextauth]";
+import { NextResponse, NextRequest } from "next/server";
 import connectDB from "@/config/db/connectDB";
 import User from "@/models/user";
-import { NextRequest, NextResponse } from "next/server";
+import { routeWrapperWithError } from "@/utils/routeWrapperWithError";
 
-export const POST = async (req: NextRequest) => {
-  const result = await UPDATE(req.body);
-  NextResponse.json(result);
-};
+export const POST = routeWrapperWithError(async (req: NextRequest) => {
+  const formData = await req.formData();
+  const data = JSON.parse(formData.get("data") as string);
+  const name = data.name;
+  const email = data.email;
+  const url = data.imgSrc.url;
+  console.log(name, email, url);
 
-const UPDATE = async (data: any) => {
-  let session = await getServerSession(authOptions);
   await connectDB();
-  if (session) {
-    data.email = session?.user?.email;
-    try {
-      await User.findOneAndUpdate(
-        { email: data.email },
-        { image: data.imageUrl },
-        { new: true }
-      );
-      return { message: "Update successful" };
-    } catch (error) {
-      console.error(error);
-      return { error: "Update failed" };
-    }
+
+  // data.email과 일치하는 사용자를 찾아서 이름과 이미지 URL을 업데이트
+  const user = await User.findOneAndUpdate(
+    { email: data.email },
+    { name: name, image: url },
+    { new: true } // 이 옵션은 업데이트된 문서를 반환
+  );
+
+  if (user) {
+    console.log(
+      "이메일이 일치하는 사용자를 찾아서 이름과 이미지 URL을 업데이트했습니다:",
+      user
+    );
+    // 업데이트된 사용자 정보를 클라이언트에게 반환
+    return NextResponse.json(user);
   } else {
-    return { error: "No session found" };
+    console.log("이메일이 일치하는 사용자를 찾지 못했습니다.");
   }
-};
+});
---
+++ src/app/mypage/Mypageview.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useState, FormEvent } from "react";
+import style from "./Mypageview.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { uploadImg2 } from "@/utils/uploadImg2";
+
+const Mypageview = () => {
+  const router = useRouter();
+  const { data: session, status, update } = useSession();
+  const [name, setName] = useState<string>("");
+  const [file, setFile] = useState<File | null>(null);
+  const [ImgSrc, setImgSrc] = useState("");
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file: File = e.target.files[0];
+      const fileUrl = await uploadImg2(file);
+      setFile(file);
+      setImgSrc(fileUrl);
+    }
+  };
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    if (file) {
+      formData.append("file", file);
+    }
+    formData.append(
+      "data",
+      JSON.stringify({
+        name: name,
+        email: session?.user?.email || "",
+        imgSrc: ImgSrc,
+      })
+    );
+
+    const res = await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+    const { fileUrl } = await res.json();
+
+    if (status === "authenticated") {
+      update({ user: { ...session.user, image: fileUrl } });
+    }
+    window.alert("변경되었습니다.");
+    router.push("/");
+  };
+  return (
+    <form onSubmit={handleSubmit} action="/api/upload" method="POST">
+      <div className={style.wrapper}>
+        <div className={style.profile_container}>
+          <div className={style.profile_img}>
+            {session && session.user && session.user.image && (
+              <img
+                src={session.user.image}
+                alt="Profile"
+                className={style.profile_image}
+              />
+            )}
+          </div>
+
+          <input
+            className={style.input_text}
+            type="text"
+            defaultValue={session?.user?.name || ""}
+            onChange={onChangeName}
+          />
+
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange} // 이미지 선택 필드에서 이미지 업데이트
+          />
+
+          <button className={style.save_btn} type="submit">
+            수정 사항 저장
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Mypageview;
