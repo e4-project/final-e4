ccb32b5fdf5ee2407cd248494e6b4a8c588375fc
catmaker <rzt0317@nate.com>
2023-12-10 05:51:35 +0900

    fix : 마이페이지 컴포넌트 이름 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Mypageview from "./Mypageview";
-const page = () => {
+const Mypage = () => {
   return (
     <div>
       <Mypageview />
@@ -8,4 +8,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Mypage;
