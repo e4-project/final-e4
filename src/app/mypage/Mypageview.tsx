50998e3d1d7a65563cec5ed0daf9c2b6f72b8d9e
catmaker <rzt0317@nate.com>
2023-12-09 14:25:32 +0900

    fix : 안쓰는 코드 제거 및 커밋용

--- src/app/mypage/Mypageview.tsx
+++ src/app/mypage/Mypageview.tsx
@@ -5,7 +5,6 @@ import style from "./Mypageview.module.css";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { uploadImg2 } from "@/utils/uploadImg2";
-import axios from "axios";
 
 const Mypageview = () => {
   const router = useRouter();
