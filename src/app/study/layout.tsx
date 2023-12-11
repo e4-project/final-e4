576e82a900b9c7a1bcca349a3b20747e8a2f2fc9
hyun0509-iva <skyblue5030@gmail.com>
2023-12-11 16:02:50 +0900

    feat: 페이지 path 전체 변경

--- src/app/study/layout.tsx
+++ src/app/study/layout.tsx
@@ -1,35 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import style from "./study.module.css";
-import React, {useState, useEffect} from 'react';
-import Link from "next/link";
-import LeftContainer from './page.view';
-const inter = Inter({ subsets: ["latin"] });
+import "@/styles/global.css";
+import Header from "@/components/common/Header";
+import AuthSession from "@/components/AuthSession.tsx/AuthSession";
 
 export const metadata: Metadata = {
-  title: "e4 | 스터디방",
+  title: "스튜",
+  description: "스터디 모집해서 사람들과 같이 스터디해봐요",
 };
 
-export default function StudyLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className={style.main_container}>
-      {/* 헤더 부분 */}
-      {/* 왼쪽 오른쪽 레이아웃 부분 */}
-      <LeftContainer />
-        <div className={style.right_container}>
-          {/* 여기서 네이게이션바  */}
-          <div className={style.link_list}>
-            <Link className={style.link_menu} href="/study">스터디 진도표</Link>
-            <Link className={style.link_menu} href="/study/note">공부 노트</Link>
-            <Link className={style.link_menu} href="/study/board" >게시판</Link>
-            <Link className={style.link_menu} href="/study/introduce">스터디 소개</Link>
-          </div>
-          <div className={style.content_warp}>{children}</div>
-      </div>
+    <div>
+      {children}
     </div>
   );
 }
