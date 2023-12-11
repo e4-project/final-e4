d5bc7fab5e4ed7fb434fec14369b682a30c5c341
hyun0509-iva <skyblue5030@gmail.com>
2023-12-11 14:32:41 +0900

    design: 버튼 스타일 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/common/Header";
 import AuthSession from "@/components/AuthSession.tsx/AuthSession";
 
 export const metadata: Metadata = {
-  title: "e4",
+  title: "스튜",
   description: "스터디 모집해서 사람들과 같이 스터디해봐요",
 };
 
