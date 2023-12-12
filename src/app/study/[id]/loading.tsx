d5746bb5eb214239c6a2a15923180445afc433c1
catmaker <rzt0317@nate.com>
2023-12-12 16:34:39 +0900

    fix : layout css 주소 수정, layout params 추가

--- src/app/study/[id]/loading.tsx
+++ src/app/study/[id]/loading.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import style from "../loadingstyle.module.css"
+import style from "./../../loadingstyle.module.css";
+
 const Loading = () => {
   // 공통 로딩중 기능 구현하기
-  return(
+  return (
     <div className={style.loading_frame}>
-        <span className={style.boll}></span>
-        <span className={style.boll}></span>
-        <span className={style.boll}></span>
+      <span className={style.boll}></span>
+      <span className={style.boll}></span>
+      <span className={style.boll}></span>
     </div>
-  )
+  );
 };
 
 export default Loading;
