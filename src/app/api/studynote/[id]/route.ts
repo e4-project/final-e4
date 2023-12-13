076e2efb10a46b7343603ed3b482f29157c483c0
catmaker <rzt0317@nate.com>
2023-12-13 17:45:00 +0900

    fix : 스터디 진도표 수정 부분 리더만 보이게 수정

--- src/app/api/studynote/[id]/route.ts
+++ src/app/api/studynote/[id]/route.ts
@@ -1,20 +1,33 @@
 import { NextResponse, NextRequest } from "next/server";
 import RecruitPost from "@/models/recruit_post";
 import { routeWrapperWithError } from "@/utils/routeWrapperWithError";
-import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import connectDB from "@/config/db/connectDB";
+import User from "@/models/user";
 
 export const GET = routeWrapperWithError(
   async (req: NextRequest, { params }: { params: { id: string } }) => {
     await connectDB();
+    const session = await getServerSession(authOptions);
+
+    const user = await User.findOne({ email: session?.user?.email });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const userId = user._id;
 
     const recruitPost = await RecruitPost.findById(params.id);
     if (!recruitPost) {
       throw new Error("RecruitPost not found");
     }
 
-    console.log(recruitPost.weekGoal); // weekGoal 필드를 콘솔에 출력
+    // console.log(recruitPost.weekGoal); // weekGoal 필드를 콘솔에 출력
 
-    return NextResponse.json({ weekGoal: recruitPost.weekGoal });
+    return NextResponse.json({
+      weekGoal: recruitPost.weekGoal,
+      result: recruitPost,
+      userId: userId,
+    });
   }
 );
