8a84398032b11ef1df9cb3e7d260e56f3088db89
catmaker <rzt0317@nate.com>
2023-12-13 17:42:47 +0900

    fix : 안쓰는 코드 제거 (진행중)

--- src/app/study/[id]/page.view.tsx
+++ src/app/study/[id]/page.view.tsx
@@ -1,63 +1,66 @@
 "use client";
-import React, {useState, useEffect} from 'react';
-import style from './study.module.css';
-import { studyInfoApi } from '@/axios/fetcher/studyhome/studyInfoApi';
-import { useRouter, usePathname } from 'next/navigation';
-import { useParams } from 'next/navigation';
-import Button from '@/components/common/Button';
+import React, { useState, useEffect } from "react";
+import style from "./study.module.css";
+import { useRouter, usePathname } from "next/navigation";
+import { useParams } from "next/navigation";
 
 const LeftContainer = () => {
-    const pathname = usePathname();
-    const [data, setData] = useState<any>(null);
-    const {id} = useParams<{id: string}>()||{};
+  const pathname = usePathname();
+  const [data, setData] = useState<any>(null);
+  const { id } = useParams<{ id: string }>() || {};
 
-    useEffect(() => {
-        if (pathname) {
-            const fetchData = async () => {
-            try {
-                const result = await fetch(`/api/study/studyinfo/${id}`);
-                console.log(result);
+  useEffect(() => {
+    if (pathname) {
+      const fetchData = async () => {
+        try {
+          const result = await fetch(`/api/study/studyinfo/${id}`);
+          console.log(result);
 
-                const data = await result.json();
-                console.log(data);
+          const data = await result.json();
+          console.log(data);
 
-                if (data) {
-                    setData({
-                        material: data.material,
-                        duration: data.duration,
-                        applicants: data.applicants,
-                        materialUrl: data.materialUrl,
-                    });
-                }
-            } catch (error) {
-                console.error('스터디 정보를 가져오는 중 에러 발생', error);
-            }
-            };
-
-            fetchData();
+          if (data) {
+            setData({
+              material: data.material,
+              duration: data.duration,
+              applicants: data.applicants,
+              materialUrl: data.materialUrl,
+            });
+          }
+        } catch (error) {
+          console.error("스터디 정보를 가져오는 중 에러 발생", error);
         }
-    }, [pathname, id]);
+      };
 
-    return (
-        <div className={style.left_container}>
-            <div className={style.left_info}>
-                <h4>함께 공부할 강의</h4>
-                <p>{data?.material}</p>
-                <div className={style.study_mini_info}>
-                    <p className={style.study_room}>스터디룸
-                        <a href={data?.materialUrl} target='_blank'>
-                        <button className={style.Entrance}>
-                            <span>드과자</span>
-                            <span>입장하기</span>
-                        </button>
-                        </a>
-                    </p>
-                    <p>스터디 기간 <span>{data?.duration}</span></p>
-                    <p>스터디 멤버 <span>{data?.applicants}</span></p>
-                </div>
-            </div>
+      fetchData();
+    }
+  }, [pathname, id]);
+
+  return (
+    <div className={style.left_container}>
+      <div className={style.left_info}>
+        <h4>함께 공부할 강의</h4>
+        <p>{data?.material}</p>
+        <div className={style.study_mini_info}>
+          <p className={style.study_room}>
+            스터디룸
+            <a href={data?.materialUrl} target="_blank">
+              <button className={style.Entrance}>
+                <span>드과자</span>
+                <span>입장하기</span>
+              </button>
+            </a>
+          </p>
+          <p>
+            스터디 기간 <span>{data?.duration}</span>
+          </p>
+          <p>
+            스터디 멤버 <span>{data?.applicants}</span>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LeftContainer;
