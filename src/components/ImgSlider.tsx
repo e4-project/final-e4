2a513ea23867295e29c18930b68ca1b305da3f84
Dong-Hoon94 <kane5178@naver.com>
2023-12-13 16:08:44 +0900

    feat: 탑버튼, 이미지슬라이드 파피콘 추가

--- src/components/ImgSlider.tsx
+++ src/components/ImgSlider.tsx
@@ -6,15 +6,19 @@ import "slick-carousel/slick/slick-theme.css";
 const ImgSlider = () => {
   const settings = {
     arrows:false,           // 양 옆에있는 next prev button
-    dots: true,             // 하단에 있는 동그라미 버튼
+    dots: true,             // 하단에 있는 동그라미 버튼                        
     infinite: true,         // 무한반복 
     speed: 500,             // 슬라이드 넘어갈때 속도 
-    slidesToShow: 1,        //한 번에 볼 수 있는 슬라이드 개수
+    slidesToShow: 1,        // 한 번에 볼 수 있는 슬라이드 개수
     slidesToScroll: 1,      // 한 번에 넘어가는 슬라이드 수 
-    autoplay: true,        //자동으로 슬라이드 넘어가게 해줌
-    autoplaySpeed: 2500,    // 위 속도
+    autoplay: true,         // 자동으로 슬라이드 넘어가게 해줌
+    autoplaySpeed: 2500,    // 슬라이드 자동 넘기기 시간(1000ms = 1초)
+    fade: true,             // 크로스페이드 모션 사용 여부 
   };
 
+   const img = [
+    
+   ]
   return (
     <Slider {...settings}>
       <div>
@@ -22,7 +26,7 @@ const ImgSlider = () => {
           src="img/1.png"
           style={{
             width: "100%",
-            height: "350px",
+            height: "350px"
   
           }}
         />
@@ -32,7 +36,7 @@ const ImgSlider = () => {
           src="img/2.png"
           style={{
             width: "100%",
-            height: "350px",
+            height: "350px"
           }}
         />
       </div>
@@ -41,7 +45,7 @@ const ImgSlider = () => {
           src="img/3.png"
           style={{
             width: "100%",
-            height: "350px",
+            height: "350px"
           }}
         />
       </div>
@@ -50,7 +54,7 @@ const ImgSlider = () => {
           src="img/4.png"
           style={{
             width: "100%",
-            height: "350px",
+            height: "350px"
           }}
         />
       </div>
