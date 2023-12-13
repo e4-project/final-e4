6df2318c13ae7380ec98ca695d81d83ce835fdff
hyun0509-iva <skyblue5030@gmail.com>
2023-12-14 03:51:12 +0900

    style: 마이스터디 path 변경  -> 서버 api과 path 일관성을 위해 /userid → /me/userid로 수정

--- src/app/mystudy/me/[userid]/page.tsx
+++ src/app/mystudy/me/[userid]/page.tsx
@@ -5,26 +5,30 @@ import MyStudyView from "./page.view";
 import { redirect } from "next/navigation";
 import { loadMystudyOneApi } from "@/axios/fetcher/mystudy/loadMystudyOneApi";
 import { loadMyApplicantApi } from "@/axios/fetcher/applicant/loadMyApplicantApi";
-// /mystudy/홍길동/applicants/리액트스터디
+import User from "@/models/user";
+/* 
+   name: 내 스터디
+   path: /mystudy/me/:userid
+   ex) /mystudy/me/657968bdd2d684ad75a2a616
+*/
 
-const Page = async () => {
+const Page = async ({ params }: { params: { userid: string } }) => {
   //TODO: MyStudy 페이지에서 로그인한 유저인지를 판단
   let session = await getServerSession(authOptions);
   if (!session) {
     console.log("인증이 필요합니다."); // 모달창으로 변경할 예정
     return redirect("/study");
   }
-  const userName = session.user?.name;
-  const myCreatedStudy: any = await loadMystudyOneApi(userName as string);
-  const myApplicants = await loadMyApplicantApi(myCreatedStudy.myStudyList[0]?.leader);
-
+  const userId = params.userid;
+  const myStudyInfo = await loadMystudyOneApi(userId);
+  const myAppliedStudy = await loadMyApplicantApi(userId);
   const data = {
-    myStudy: myCreatedStudy.myStudyList,
-    myApplicants: [...myApplicants]
-  }
+    myCreatedStudy: myStudyInfo?.createdMyStudy,
+    myAppliedStudy,
+  };
   return (
     <div>
-      <MyStudyView data={data}/>
+      <MyStudyView data={data} />
     </div>
   );
 };
--- src/axios/fetcher/recruit/loadRecruitOneApi.ts
+++ src/axios/fetcher/recruit/loadRecruitOneApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const loadRecruitOneByIdApi = async (id: string) => {
   try {
-    //내가 만든 스터디 leader 이름으로 찾기
+    //내가 만든 스터디 leader _id으로 찾기
     const {data} = await axios.get(
       getBaseUrl(`/api/recruits/id/${id}`)
     );
@@ -13,14 +13,14 @@ export const loadRecruitOneByIdApi = async (id: string) => {
   }
 };
 
-export const loadRecruitOneByNameApi = async (studyName: string) => {
-  try {
-    //내가 만든 스터디 leader 이름으로 찾기
-    const {data} = await axios.get(
-      getBaseUrl(`/api/recruits/studyname/${studyName}`)
-    );
-    return data;
-  } catch(error: any) {
-    console.error(error.response)
-  }
-};
+// export const loadRecruitOneByNameApi = async (studyName: string) => {
+//   try {
+//     //내가 만든 스터디 이름으로 찾기
+//     const {data} = await axios.get(
+//       getBaseUrl(`/api/recruits/studyname/${studyName}`)
+//     );
+//     return data;
+//   } catch(error: any) {
+//     console.error(error.response)
+//   }
+// };
