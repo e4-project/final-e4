ba5f79be1ba147e595b14d01021435b4ececbf34
hyun0509-iva <skyblue5030@gmail.com>
2023-12-18 04:14:31 +0900

    fix: 로딩중인 때 모집글 버튼 나오는 현상 해결

--- src/app/recruit/[slug]/page.view.tsx
+++ src/app/recruit/[slug]/page.view.tsx
@@ -1,6 +1,13 @@
 "use client";
 import Link from "next/link";
-import { useState, ChangeEvent, FormEvent, useEffect } from "react";
+import {
+  useState,
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useCallback,
+} from "react";
+import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
 import { IResponseRecruitPost } from "@/interfaces/recruit";
 import CommentForm from "@/components/Comment/CommentForm";
@@ -9,25 +16,32 @@ import CustomModal from "@/components/common/modal/Custom/page";
 import modalStyle from "@/components/common/modal/modal.module.css";
 import SingleComment from "@/components/Comment/SingleComment";
 import { IResponseUser } from "@/interfaces/user";
-import { loadUserApi } from "@/axios/fetcher/user/loadUserApi";
 import {
   loadRecruitComment,
   deleteRecruitComment,
   postRecruitComment,
 } from "@/axios/fetcher/recruitComment";
-import style from "./recruit.module.css";
+import { loadUserApi } from "@/axios/fetcher/user/loadUserApi";
 import { postApplicantApi } from "@/axios/fetcher/applicant/postApplicantApi";
-import dayjs from "dayjs";
+import {
+  deleteRecruitLike,
+  postRecruitLikes,
+} from "@/axios/fetcher/recruitLikes";
 import { isDeadLine } from "@/utils/isDeadLine";
+import style from "./recruit.module.css";
 
 interface IProps {
   data: IResponseRecruitPost;
+  likesData: any;
 }
-export default function StudyPageView({ data }: IProps) {
+export default function StudyPageView({ data, likesData }: IProps) {
   const [message, setMessage] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
   const [isApplicant, setIsApplicant] = useState<null | boolean>(null);
+  const [isLoadingAppliCant, setIsLoadingAppliCant] = useState(true);
   const [currentUser, setCurrentUser] = useState<IResponseUser | null>(null);
+  const [isLikedRecruit, setIsLikedRecruit] = useState(false);
+  const [isLoadingLikedRecruit, setLoadingLikedRecruit] = useState(true);
   const deadLine = new Date(data?.deadLine).getTime();
   const router = useRouter();
   useEffect(() => {
@@ -39,12 +53,45 @@ export default function StudyPageView({ data }: IProps) {
 
   useEffect(() => {
     if (data && currentUser) {
-      const isApplicant = data?.applicants.includes(currentUser?._id as string);
-      setIsApplicant(isApplicant);
+      const isApplicantData = data?.applicants.includes(
+        currentUser?._id as string
+      );
+      setIsApplicant(isApplicantData);
+      setIsLoadingAppliCant(false);
     }
   }, [currentUser, data]);
 
+  useEffect(() => {
+    likesData?.likes?.map((like: any) => {
+      if (like.userId === currentUser?._id) {
+        setIsLikedRecruit(like.userId === currentUser?._id);
+      }
+    });
+  }, [currentUser?._id, likesData?.likes]);
+
+  const fetchLike = useCallback(async () => {
+    if (!isLikedRecruit) {
+      await postRecruitLikes(data._id, currentUser?._id as string);
+      setLoadingLikedRecruit(false);
+      router.refresh();
+      return;
+    } else {
+      await deleteRecruitLike(data._id);
+      setLoadingLikedRecruit(false);
+      setIsLikedRecruit(false)
+      router.refresh();
+    }
+    if (isLoadingLikedRecruit) return;
+  }, [
+    currentUser?._id,
+    data._id,
+    isLikedRecruit,
+    isLoadingLikedRecruit,
+    router,
+  ]);
+
   const showModal = () => {
+    if (isLoadingAppliCant) return; //로딩시 신청 모달창 안나오게
     setModalOpen(true);
   };
 
@@ -71,6 +118,22 @@ export default function StudyPageView({ data }: IProps) {
     closeModal();
     setMessage("");
   };
+
+  const btnActionCommonUser = (() => {
+    if (currentUser) {
+      return isLoadingAppliCant
+        ? "로딩중..."
+        : (isApplicant as boolean)
+        ? "이미 신청한 스터디"
+        : "스터디 참여 신청";
+    } else {
+      return "스터디 참여 신청";
+    }
+  })();
+
+  const isRecruitUserTheCurrentUser =
+    currentUser && data?.leader?._id === currentUser?._id;
+  console.log(isLikedRecruit);
   return (
     <div className={style.sheet}>
       {modalOpen && (
@@ -133,7 +196,7 @@ export default function StudyPageView({ data }: IProps) {
               <img src="/icons/icon_time.svg" alt="" />
               <span> 모집 마감</span>
               <span className={style.font_bold}>
-              {dayjs(data?.deadLine).format("MM월 DD일")}
+                {dayjs(data?.deadLine).format("MM월 DD일")}
               </span>
             </li>
             <li className={style.list}>
@@ -141,7 +204,7 @@ export default function StudyPageView({ data }: IProps) {
                 <div className={style.dead_line}>
                   <span>{isDeadLine(deadLine) ? "모집 마감" : "모집중"}</span>
                 </div>
-                {currentUser && data?.leader?._id === currentUser?._id ? (
+                {isRecruitUserTheCurrentUser ? (
                   <button
                     className={style.application_button}
                     type="button"
@@ -161,20 +224,21 @@ export default function StudyPageView({ data }: IProps) {
                     // 현재 유저가 해당 모집글 참여 신청자가 아닌경우만 신청
                     disabled={isApplicant as boolean}
                   >
-                    {isApplicant === null
-                      ? "로딩중..."
-                      : (isApplicant as boolean)
-                      ? "이미 신청한 스터디"
-                      : "스터디 참여 신청"}
+                    {btnActionCommonUser}
                   </button>
                 )}
                 <input
                   className={style.good_check}
                   type="checkbox"
+                  checked={isLikedRecruit}
                   name=""
                   id="good"
                 />
-                <label className={style.good_label} htmlFor="good">
+                <label
+                  className={style.good_label}
+                  htmlFor="good"
+                  onClick={fetchLike}
+                >
                   <div className={style.good}>
                     좋아요
                     <img src="/icons/icon_like.svg" alt="" />
@@ -185,6 +249,9 @@ export default function StudyPageView({ data }: IProps) {
                   <div className={style.box}></div>
                   <div className={style.box}></div>
                 </label>
+                <div className={style.like_count}>
+                  <span>좋아요 {likesData.count}개</span>
+                </div>
               </div>
             </li>
           </ul>
