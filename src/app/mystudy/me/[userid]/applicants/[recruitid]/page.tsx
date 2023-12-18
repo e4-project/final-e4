b4c8c452c461a17d0da3754b796fae7d306c3551
hyun0509-iva <skyblue5030@gmail.com>
2023-12-18 20:47:41 +0900

    feat: 멤버 데이터 추가  : 이 데이터로 스터디 마감 표시 및 활성화

--- src/app/mystudy/me/[userid]/applicants/[recruitid]/page.tsx
+++ src/app/mystudy/me/[userid]/applicants/[recruitid]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ApplicantsView from "./page.view";
 import { loadApplicantOfMyStudyApi } from "@/axios/fetcher/applicant";
+import { loadMystudyMemberApi } from "@/axios/fetcher/mystudy";
 
 /* 
    path: /mystudy/me/:userid/applicants/:recruitid
@@ -11,9 +12,12 @@ const Page = async ({ params }: { params: { userid: string, recruitid: string } 
   // 스터디 참여 신청자
   console.log("스터디 참여 신청자")
   const data = await loadApplicantOfMyStudyApi(userId, recruitId)
+  // 승인된 멤버는 member에서 가져오는게 좋다고 판단. 
+  const members = await loadMystudyMemberApi(userId, recruitId)
+  console.log({data, members})
   return (
     <div>
-      <ApplicantsView data={data}/>
+      <ApplicantsView data={data} members={members}/>
     </div>
   );
 };
