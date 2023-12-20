ef56cf23577d9dd0b626f86296d0c9dc846297a3
RangCloud <mtr008@naver.com>
2023-12-20 14:36:01 +0900

    fix : 개인 노트 출력 적용

--- src/app/api/study/note/[id]/route.ts
+++ src/app/api/study/note/[id]/route.ts
@@ -6,6 +6,8 @@ import StudyNote from "@/models/study_note";
 import User from "@/models/user";
 import { useSearchParams } from "next/navigation";
 
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 interface NextRequest {
   // 기존 속성들...
   url: string;
@@ -26,26 +28,32 @@ export const GET = routeWrapperWithError(
     console.log(week);
 
     console.log(params.id);
-      // 유저 정보 가져오기
+    // 유저 정보 가져오기
     const user = await User.findOne();
-    console.log({user});
     
+    console.log({ user });
+
     try {
       // id와 주차 정보와 관련된 노트 찾고
       const studyNote = await StudyNote.findOne({
         author: params.id,
         week: week,
       });
-      console.log({studyNote});
-
-      
+      console.log({ studyNote });
 
       if (studyNote) {
         // 찾은 노트의 contents 가져옴
-        return NextResponse.json({ memberId: studyNote.author, contents: studyNote.contents, week: studyNote.week });
-        } else {
-        return NextResponse.json({ error: '해당 노트 내용 없음' }, { status: 404 });
-        }
+        return NextResponse.json({
+          memberId: studyNote.author,
+          contents: studyNote.contents,
+          week: studyNote.week,
+        });
+      } else {
+        return NextResponse.json(
+          { error: "해당 노트 내용 없음" },
+          { status: 404 }
+        );
+      }
     } catch (error) {
       console.error("조회 오류", error);
       return NextResponse.json({ error: "서버 오류" }, { status: 500 });
@@ -55,27 +63,29 @@ export const GET = routeWrapperWithError(
 export const POST = routeWrapperWithError(
   async (req: NextRequest, { params }: { params: { id: string } }) => {
     const data = await req.json();
+    const session = await getServerSession(authOptions);
 
     try {
       await connectDB();
+      const user = await User.findOne({ email: session?.user?.email });
 
       const { week, contents } = data;
 
       const studyId = params.id;
 
       const newNote = new StudyNote({
-        /* author: user._id, */
+        author: user._id,
         studyId: studyId,
         week: week,
         contents: contents,
       });
 
+      // console.log({ newNote });
       const savedNote = await newNote.save();
 
       return NextResponse.json({ success: true, note: savedNote });
     } catch (error) {
       console.error("저장 오류", error);
-
       return NextResponse.json(
         { success: false, error: "서버 오류" },
         { status: 500 }
